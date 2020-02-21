2c7c45c65a6017f8fffa7e833dba1156537598df
Bill Rawlinson <bill.rawlinson@uspsector.com>
2020-02-21 14:18:41 -0500

    typescript package.json and  move eslint cfg
    typescript libraries added to install list
    moved eslint config purely as preference

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/standard',
+    '@vue/typescript'
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
+      }
+    }
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+}
