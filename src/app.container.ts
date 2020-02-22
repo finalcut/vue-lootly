2ea12e34cd2dd3b17db37ebfb9f1e9990a8b1fc9
Bill Rawlinson <bill@rawlinson.us>
2020-02-21 19:35:54 -0500

    setup initial inversify code restructure main.ts

---
+++ src/app.container.ts
@@ -0,0 +1,5 @@
+import 'reflect-metadata'
+// import { container } from 'inversify-props'
+
+export default function buildDependencyContainer (): void {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { Vue } from 'vue-property-decorator'
+import App from '@/App.vue'
 import vuetify from '@/plugins/vuetify'
+import buildDependencyContainer from '@/app.container'
 
 Vue.config.productionTip = false
 
-new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+class AppBootstrap {
+  constructor () {
+    this.loadDependencyContainer()
+    this.loadVueApp()
+  }
+
+  private loadDependencyContainer (): void {
+    buildDependencyContainer()
+  }
+
+  private loadVueApp (): void {
+    Vue.config.productionTip = false
+
+    new Vue({
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+}
+
+const app = new AppBootstrap()
+export default app
