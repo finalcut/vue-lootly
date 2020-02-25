a652935a728a54bdba3704a5702116897514d0b3
Bill Rawlinson <bill@rawlinson.us>
2020-02-25 14:01:26 -0500

    define the book component

--- src/App.vue
+++ src/App.vue
@@ -55,17 +55,27 @@
 <script lang="ts">
 // these decorators are used all over the place in our typescript app.
 import { Component, Vue } from 'vue-property-decorator'
+// we want to inject the service into this file
+import { inject } from 'inversify-props'
+import { Registry } from './Registry'
+import IBookService from './services/IBookService'
+import Book from './components/book/Book.vue'
 
-    // almost everything we create will be a component
-    // vue apps are components made up of other components
-    @Component({
-      name: 'lootly'
-    })
-
+// almost everything we create will be a component
+// vue apps are components made up of other components
+@Component({
+  name: 'lootly',
+  components: {
+    book: Book
+  }
+})
 export default class extends Vue {
   // this is a property getter.  Typescript lets us say what type of
   // value will be returned (string).  It's used up in the template as {{ AppTitle }}
   get AppTitle (): string { return 'lootly' }
+
+  @inject(Registry.IBookService)
+  private bookService!: IBookService
 
   // some bogus properties to act as placeholders for the layout we're starting with
   private drawer: boolean = false
---
+++ src/components/book/Book.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="model">
+    <h1>{{ model.title }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { inject } from 'inversify-props'
+import { Registry } from '@/Registry'
+import IBook from '@/models/IBook'
+import IBookService from '@/services/IBookService'
+
+@Component({
+  name: 'book'
+})
+export default class extends Vue {
+  @Prop(Object) readonly model!: IBook
+
+  @inject(Registry.IBookService)
+  private bookService!: IBookService
+};
+</script>
